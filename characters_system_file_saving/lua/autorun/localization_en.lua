7ebac5e52976de6454ada67ba3a64da7e20adf54
Soundwave <89224732+5oundwave@users.noreply.github.com>
2025-04-24 16:46:29 +0300

    Delete characters_system_file_saving directory

--- characters_system_file_saving/lua/autorun/localization_en.lua
+++
@@ -1,8 +0,0 @@
-if CLIENT then
-    local LANG = {}
-    LANG["hp_format"]     = "HP: %d/%d"
-    LANG["armor_format"]  = "Armor: %d/%d"
-    LANG["mode_hp"]       = "HP Restore Mode"
-    LANG["mode_armor"]    = "Armor Restore Mode"
-    return LANG
-end
